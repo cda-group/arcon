dd3aafa099fa7dc67687c5b843d120cbe82d1387
Max Meldrum <max@meldrum.se>
2019-06-19 10:55:55 +0200

    Add helpers for window msg

--- arcc/runtime/messages/src/protobuf.rs
+++ arcc/runtime/messages/src/protobuf.rs
@@ -2,3 +2,23 @@ extern crate protobuf;
 
 pub use protobuf::messages::*;
 pub use protobuf::ProtoSer;
+
+pub fn create_window_trigger() -> WindowMessage {
+    let mut msg = WindowMessage::new();
+    let trigger = WindowTrigger::new();
+    msg.set_trigger(trigger);
+    msg
+}
+
+pub fn create_window_complete() -> WindowMessage {
+    let mut msg = WindowMessage::new();
+    let complete = WindowComplete::new();
+    msg.set_complete(complete);
+    msg
+}
+
+pub fn create_windowed_element(element: Element) -> WindowMessage {
+    let mut msg = WindowMessage::new();
+    msg.set_element(element);
+    msg
+}
