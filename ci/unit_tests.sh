d062e45522205986c641e71ffaaa0b4bae2984e3
Max Meldrum <max@meldrum.se>
2019-07-28 14:10:12 +0200

    [CI] fix hopefully

--- ci/unit_tests.sh
+++ ci/unit_tests.sh
@@ -3,7 +3,7 @@ set -e
 
 NIGHTLY_VER="nightly-2019-07-04"
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain $NIGHTLY_VER
 export PATH=$PATH:$HOME/.cargo/bin
 rustup toolchain install $NIGHTLY_VER
 rustup default $NIGHTLY_VER
@@ -12,6 +12,6 @@ rustc --version
 ./ci/install_capnproto.sh
 ./ci/install_llvm.sh
 export RUST_BACKTRACE=1
-cd arcc && cargo +nightly test --all
+cd arcc && cargo +$NIGHTLY_VER test --all
 #cd ../ && ./ci/install_sbt.sh
 #cd operational-plane && sbt test
