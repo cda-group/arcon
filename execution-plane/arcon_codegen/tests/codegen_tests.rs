3ed2fa2058c56cc95be78da0aaaec0329f8c7de2
Max Meldrum <max@meldrum.se>
2019-08-14 14:04:26 +0200

    [Rust] Add trybuild, closes #31

--- execution-plane/arcon_codegen/tests/codegen_tests.rs
+++ execution-plane/arcon_codegen/tests/codegen_tests.rs
@@ -1,25 +1,39 @@
 extern crate arcon;
 extern crate arcon_codegen;
-extern crate compiletest_rs as compiletest;
 
 use arcon_codegen::*;
 use arcon_spec::*;
 use std::fs;
-use std::path::PathBuf;
 
 pub const RUN_PASS_MODE: &str = "run-pass";
 pub const RUN_PASS_PATH: &str = "tests/run-pass";
 pub const SPECIFICATION_PATH: &str = "tests/specifications";
 
-fn run_mode(mode: &str) {
-    let mut config = compiletest::Config::default().tempdir();
-    let cfg_mode = mode.parse().expect("Invalid mode");
+#[test]
+fn codegen_test() {
+    // makes sure that tests/run-pass does not exist
+    let _ = fs::remove_dir_all(RUN_PASS_PATH);
 
-    config.mode = cfg_mode;
-    config.src_base = PathBuf::from(format!("tests/{}", mode));
-    config.target_rustcflags = Some("-L ../target/debug -L ../target/debug/deps".to_string());
+    // Fresh start of run-pass tests
+    fs::create_dir_all(RUN_PASS_PATH).unwrap();
 
-    compiletest::run_tests(&config);
+    let t = trybuild::TestCases::new();
+
+    add_test_spec("basic_dataflow");
+    add_test_spec("tumbling_window_dataflow");
+    add_test_spec("file_source_sink");
+
+    // test all generated .rs files
+    let specs = format!("{}/{}", RUN_PASS_PATH, "*.rs");
+    t.pass(&specs);
+}
+
+fn add_test_spec(name: &str) {
+    let json_path = format!("{}/{}.json", SPECIFICATION_PATH, name);
+    let spec = ArconSpec::load(&json_path).unwrap();
+    let generated_code = generate(&spec, true).unwrap();
+    let path = format!("{}/{}.rs", RUN_PASS_PATH, name);
+    let _ = to_file(generated_code, path.to_string());
 }
 
 fn _add_empty_main(path: &str) {
@@ -34,29 +48,4 @@ fn _add_empty_main(path: &str) {
         .unwrap();
 
     let _ = writeln!(file, "{}", main);
-}
-
-#[test]
-fn codegen_test() {
-    // makes sure that tests/run-pass does not exist
-    let _ = fs::remove_dir_all(RUN_PASS_PATH);
-
-    // Fresh start of run-pass tests
-    fs::create_dir_all(RUN_PASS_PATH).unwrap();
-
-    add_test_spec("basic_dataflow");
-    add_test_spec("tumbling_window_dataflow");
-    add_test_spec("file_source_sink");
-
-    run_mode(RUN_PASS_MODE);
-
-    fs::remove_dir_all(RUN_PASS_PATH).unwrap();
-}
-
-fn add_test_spec(name: &str) {
-    let json_path = format!("{}/{}.json", SPECIFICATION_PATH, name);
-    let spec = ArconSpec::load(&json_path).unwrap();
-    let generated_code = generate(&spec, true).unwrap();
-    let path = format!("{}/{}.rs", RUN_PASS_PATH, name);
-    let _ = to_file(generated_code, path.to_string());
 }
