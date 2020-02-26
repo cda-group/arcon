8d1a477be70ef3be9117b4d92ac333631a4dfe8a
Max Meldrum <max@meldrum.se>
2020-02-26 10:39:09 +0100

    Add allocator (#76)

---
+++ execution-plane/arcon/src/allocator/mod.rs
@@ -0,0 +1,132 @@
+// Copyright (c) 2020, KTH Royal Institute of Technology.
+// SPDX-License-Identifier: AGPL-3.0-only
+
+use arcon_error::*;
+use std::alloc::{GlobalAlloc, Layout, System};
+use std::collections::HashMap;
+use uuid::Uuid;
+
+/// Type alias for a unique Alloc identifier
+pub type AllocId = Uuid;
+/// Type alias for alloc pointers
+pub type AllocPtr = *mut u8;
+
+/// An Allocator for [arcon]
+///
+/// The allocator is not meant to handle all heap allocations
+/// during the execution. However, it is intended to be used to manage
+/// memory for different sections of [arcon]. This includes message buffers,
+/// network buffers, state backends...
+#[derive(Debug)]
+pub struct ArconAllocator {
+    /// HashMap keeping track of allocations
+    allocations: HashMap<AllocId, (AllocPtr, Layout)>,
+    /// Memory limit
+    limit: usize,
+    /// Total allocations made so far
+    alloc_counter: u64,
+    /// Bytes allocated currently
+    curr_alloc: usize,
+}
+
+impl ArconAllocator {
+    /// Creates a new ArconAllocator with the given memory limit size
+    pub fn new(limit: usize) -> ArconAllocator {
+        ArconAllocator {
+            allocations: HashMap::new(),
+            limit,
+            alloc_counter: 0,
+            curr_alloc: 0,
+        }
+    }
+    /// Allocate memory block of type T with given capacity
+    pub unsafe fn alloc<T>(&mut self, capacity: usize) -> ArconResult<(AllocId, AllocPtr)> {
+        if capacity == 0 {
+            return arcon_err!("{}", "Cannot alloc for 0 sized pointer");
+        }
+        let (size, align) = (std::mem::size_of::<T>(), std::mem::align_of::<T>());
+        let required_bytes = size * capacity;
+
+        if self.curr_alloc + required_bytes > self.limit {
+            return arcon_err!("{}", "OutOfMemory");
+        }
+
+        let layout = Layout::from_size_align_unchecked(required_bytes, align);
+        let mem = System.alloc(layout);
+        self.curr_alloc += layout.size();
+        self.alloc_counter += 1;
+        let id = AllocId::new_v4();
+        self.allocations.insert(id, (mem, layout));
+
+        Ok((id, mem))
+    }
+    /// Deallocate memory through the given AllocId
+    pub unsafe fn dealloc(&mut self, id: AllocId) {
+        if let Some((ptr, layout)) = self.allocations.remove(&id) {
+            System.dealloc(ptr, layout);
+            self.curr_alloc -= layout.size();
+        }
+    }
+    /// Returns amount of bytes that are currently allocated
+    pub fn allocated_bytes(&self) -> usize {
+        self.curr_alloc
+    }
+    /// Returns total allocations made so far
+    pub fn total_allocations(&self) -> u64 {
+        self.alloc_counter
+    }
+    /// Returns how much bytes are available to allocate
+    pub fn bytes_remaining(&self) -> usize {
+        self.limit - self.curr_alloc
+    }
+}
+
+impl Drop for ArconAllocator {
+    fn drop(&mut self) {
+        // Just to make sure we are not leaving any outstanding allocations
+        let ids: Vec<AllocId> = self.allocations.keys().map(|i| i.clone()).collect();
+        for id in ids {
+            unsafe {
+                self.dealloc(id);
+            }
+        }
+    }
+}
+
+unsafe impl Send for ArconAllocator {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::{Arc, Mutex};
+
+    #[test]
+    fn simple_allocator_test() {
+        let total_bytes = 1024;
+        let allocator = Arc::new(Mutex::new(ArconAllocator::new(total_bytes)));
+
+        let mut a = allocator.lock().unwrap();
+
+        assert_eq!(a.allocated_bytes(), 0);
+
+        let (id_one, _) = unsafe { a.alloc::<u64>(100).unwrap() };
+        // 100 * 8
+        assert_eq!(a.allocated_bytes(), 800);
+
+        let (id_two, _) = unsafe { a.alloc::<i32>(50).unwrap() };
+        // 50 * 4
+        assert_eq!(a.allocated_bytes(), 1000);
+
+        // At this point, we will receive an out-of-memory error..
+        assert_eq!(unsafe { a.alloc::<f32>(500).is_err() }, true);
+
+        // dealloc
+        unsafe { a.dealloc(id_one) };
+        unsafe { a.dealloc(id_two) };
+
+        // final assertations
+        assert_eq!(a.allocated_bytes(), 0);
+        assert_eq!(a.total_allocations(), 2);
+        assert_eq!(a.bytes_remaining(), total_bytes);
+    }
+}
