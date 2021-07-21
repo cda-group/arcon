090f3411d503aafe3e7e4c5a9a5c7cb90326ec5d
Max Meldrum <max@meldrum.se>
2021-07-21 13:48:22 +0200

    add initial website

---
+++ website/README.md
@@ -0,0 +1,4 @@
+# Arcon Website
+
+Created using [smooth-doc](https://github.com/gregberge/smooth-doc)
+
---
+++ website/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "smooth-doc",
+      options: {
+        name: 'Arcon',
+        slug: 'arcon',
+        title: 'Hej',
+        author: 'Arcon Developers',
+        description: 'Runtime for real-time analytics applications in Rust',
+        siteUrl: "https://example.com",
+        github: 'https://github.com/cda-group/arcon',
+        githubRepositoryURL: 'https://github.com/cda-group/arcon',
+        navItems: [{ title: 'Learn', url: '/learn/' }],
+        sections: ['Arcon', 'Guide', 'Advanced'],
+      },
+    },
+  ],
+};
