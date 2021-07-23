5c80ea94df7c04bbdf9bcb996f97eda4c0a4bc41
Sanskar95 <snskr95@gmail.com>
2021-07-23 12:55:30 +0530

    syncing kafka_source with master to avoid conflicts

--- guide/examples/src/bin/kafka_source.rs
+++ guide/examples/src/bin/kafka_source.rs
@@ -1,31 +1,21 @@
 use arcon::prelude::*;
 
 fn main() {
-    let mut perf_events = PerfEvents::new();
-    perf_events.add(HardwareCounter::CPU_CYCLES);
-    perf_events.add(HardwareCounter::CACHE_MISSES);
-
     let consumer_conf = KafkaConsumerConf::default()
         .with_topic("test")
         .set("group.id", "test")
         .set("bootstrap.servers", "localhost:9092")
         .set("enable.auto.commit", "false");
 
-    let mut pipeline = Pipeline::default()
-        .kafka(consumer_conf, JsonSchema::new(), 1, |conf| {
+    let mut app = Application::default()
+        .kafka(consumer_conf, JsonSchema::new(), 2, |conf| {
             conf.set_arcon_time(ArconTime::Event);
             conf.set_timestamp_extractor(|x: &u64| *x);
         })
-        .operator(OperatorBuilder {
-            constructor: Arc::new(|_| Map::new(|x| x + 10)),
-            conf: OperatorConf {
-                perf_events,
-                ..Default::default()
-            },
-        })
+        .map(|x| x + 10)
         .to_console()
         .build();
 
-    pipeline.start();
-    pipeline.await_termination();
+    app.start();
+    app.await_termination();
 }
